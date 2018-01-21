ce649fdedeb8df17f21ac11362dc75c6cfec8571
Juan Erazo <fersot100@yahoo.com>
2018-01-20 21:12:32 -0800

    Preliminary testing on object parsing function

--- parser.js
+++ parser.js
@@ -6,22 +6,23 @@ if (process.argv.length !== 3) {
      console.log("You must specify one file");
      process.exit(1);
 }
+
 const fs = require('fs'), filename = process.argv[2];
 let objects = strings = arrays = 0;
 
+const _regexlib = {
+    property_or_number: /(:[\w-./]+)|(\d+\s)/,
+    property: /(:[\w-]+)/,
+    property_or_object_close: /(:[\w-]+)|}/,
+    space_before_item: /\s[":{[]/
+}
+
 //This is the main function that parses the file
 function parseOrcbrew(text) {
-
-    //Copy text to local content variable
-    let content = text.slice(0);
-
     //While there is still content, we continue to look for structures and parse them
-    do {
-        content = findNextStructure(content.slice(1));
-        parseByType(content);
-    } while (content !== "");
-
-    console.log(`Found ${objects} objects, ${arrays} arrays, and ${strings} strings.\n`);
+    parseByType(text.trim());
+    console.log(`Found ${objects} objects, 
+    ${arrays} arrays, and ${strings} strings.\n`);
 }
 
 function parseByType(string){
@@ -38,37 +39,82 @@ function parseByType(string){
             arrays++;
             return parseArray(string);
             break;
+        case ':':
+            return parseProperty(string);
     }
 }
 
+//Returns an actual object with all properties parsed
 function parseObject(string){
-    return {};
+    console.log("Parsing Object");
+    //Slice the text after the object end
+    let object = {}, key = "";
+    let objectString = string.slice(0, findCollectionClose(string) + 1);
+    if (objectString.length > 2) {
+        while(objectString.length > key.length + 4) {
+            key = objectString.match(_regexlib.property_or_number)[0].slice(1);
+            objectString = findNextObjectValue(objectString);
+            object[key] = parseByType(objectString);
+            if(objectString.match(_regexlib.property_or_object_close)[0] === '}')
+                break;
+            objectstring = objectString.slice(string.search(_regexlib.property));
+        }
+    }
+    return object;
 }
 
+function findNextObjectValue(string) {
+    return string.slice(string.search(_regexlib.space_before_item) + 1);
+}
+
+function findNextArrayItem(string){
+    return string.slice(string.search(_regexlib.space_before_item) + 1);
+}
+
+//Returns an actual javscript string value
 function parseString(string){
-    return "";
+    console.log("Parsing String");
+    return string.slice(1).slice(0,string.search(/"/));
 }
 
+//Returns an actual javascript array with all elements parsed
 function parseArray(string){
+    console.log("Parsing Array");
     const array = [];
-    const close = findStructureClose(string);
+    const close = findCollectionClose(string);
     string = string.slice(0, close);
-    let next = string.skipSpace(string.slice(1));
-    array.append(parseByType(string));
-
-
-
+    while(string > 2){
+        console.log(string.length);
+        array.append(parseByType(string));
+        string = findNextObjectValue(string);
+    }
     return array;
 }
 
-function findStructureClose(string){
+//Returns a string with the property name
+function parseProperty(string){
+    return string.match(_regexlib.property).slice(1);
+}
+
+function findCollectionClose(string){
     //string must start with the opening symbol of the collection
     let pairs = 0, open, close;
-    if(string.charAt(0) === '{') [open, close] = ['{', '}'];
-    else if(string.charAt(0) === '[') [open, close] = ['[', ']'];
+    
+    switch (string.charAt(0)) {
+        case '{':
+            [open, close] = ['{', '}'];
+            break;
+        case '[':
+            [open, close] = ['[', ']'];
+            break;
+        default:
+            throw new Error(`findCollectionClose() must 
+            take a string where the first character is { or [.\n
+            string received with first character ${string.charAt(0)}`);
+    }
     
     //Counting pairs accounts for nested collections
-    for(let i = 0; i < string.length; i++){
+    for(let i = 1; i < string.length; i++){
         switch (string.charAt(i)){
             case open:
                 pairs++;
@@ -79,7 +125,8 @@ function findStructureClose(string){
                 break;
         }
     }
-    throw error(`No closing ${close} for structure ${string.slice(0, 10)}`);
+
+    throw new Error(`No closing ${close} for structure ${string.slice(0, 15)}`);
 }
 
 
@@ -98,8 +145,7 @@ function skipSpace(string){
 }
 
 fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log('Reading from [' + filename + ']...\nFile is ' 
-        + data.length + ' characters long');
+    if (err) {throw err;}
+    console.log('Reading from [' + filename + ']...\nFile is ' + data.length + ' characters long');
     parseOrcbrew(data);
 })
